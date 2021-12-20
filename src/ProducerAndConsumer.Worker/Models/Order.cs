b8560d0267e6587491bc24f50d7962959a3a5e47
Fernando Nogueira <fgeraldo11@gmail.com>
2021-12-20 10:46:23 -0300

    Implementação do consumidor

---
+++ src/ProducerAndConsumer.Worker/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace ProducerAndConsumer.Worker.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"OrderId: {OrderId}|OrderId: {Title}|OrderId: {Description}|OrderId: {Price}";
+        }
+    }
+}
