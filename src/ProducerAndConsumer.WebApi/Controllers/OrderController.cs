a389783cac2b051a9b58865a1f7bc3c49e542b58
Fernando Nogueira <fgeraldo11@gmail.com>
2021-12-20 09:59:26 -0300

    Adicionada API produtor

---
+++ src/ProducerAndConsumer.WebApi/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProducerAndConsumer.WebApi.Models;
+using System;
+
+namespace ProducerAndConsumer.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        public readonly ILogger<OrderController> _logger;
+
+        public OrderController(ILogger<OrderController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult InsertOrder([FromBody]Order order)
+        {
+            try
+            {
+                return Accepted(order);
+            }
+            catch(Exception e)
+            {
+                _logger.LogError($"Ocorreu ao tentar criar uma nova ordem.", e);
+                
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
---
+++ src/ProducerAndConsumer.WebApi/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace ProducerAndConsumer.WebApi.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+    }
+}
